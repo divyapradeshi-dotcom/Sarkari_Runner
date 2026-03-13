f46a0dc4b28f30f38852a0a606dd291aa61fee7d
divyapardeshi <divyapradeshi@gmail.com>
2026-03-13 14:33:47 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-500">
+Hello Tailwind
+</h1>
+    </div>
+  )
+}
+
+export default App
